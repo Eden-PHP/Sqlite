a03b8725d2a3119d5539d17857948b25e2cd71ea
Christian Blanquera <cblanquera@gmail.com>
2013-09-24 09:43:56 +0800

    changing from framework structure to component structure

--- Tests/Sqlite/ModelTest.php
+++ Tests/Sqlite/ModelTest.php
@@ -7,13 +7,13 @@
  * distributed with this package.
  */
  
-class Eden_Tests_Sqlite_ModelTest extends \PHPUnit_Framework_TestCase
+class Eden_Sqlite_Tests_Sqlite_ModelTest extends \PHPUnit_Framework_TestCase
 {
 	public static $database;
 	
 	public function setUp() {
 		date_default_timezone_set('GMT');
-		self::$database = eden('sqlite', realpath(__DIR__.'/../../../assets').'/unit.db');
+		self::$database = eden('sqlite', realpath(__DIR__.'/../assets').'/unit.db');
 	}
 	
 	/* FACTORY METHODS */
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,11 +7,11 @@
  * distributed with this package.
  */
  
-require_once __DIR__.'/../../Core/library/Eden/Core/Loader.php';
+require_once __DIR__.'/../../Core/Loader.php';
 Eden\Core\Loader::i()
 	->addRoot(true, 'Eden\\Core')
-	->addRoot(realpath(__DIR__.'/../../Utility/library')	, 'Eden\\Utility')
-	->addRoot(realpath(__DIR__.'/../../Sql/library')		, 'Eden\\Sql')
-	->addRoot(realpath(__DIR__.'/../../Sqlite/library')		, 'Eden\\Sqlite')
+	->addRoot(realpath(__DIR__.'/../..'), 'Eden\\Utility')
+	->addRoot(realpath(__DIR__.'/../..'), 'Eden\\Sql')
+	->addRoot(realpath(__DIR__.'/../..'), 'Eden\\Sqlite')
 	->register()
 	->load('Controller');
